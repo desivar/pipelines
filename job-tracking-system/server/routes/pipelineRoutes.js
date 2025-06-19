43c5f65d62a03705f278ca884c899e1d85ad5766
desivar <jillian_desire@yahoo.es>
2025-06-19 09:15:08 -0600

    remove auth to simplicity

--- job-tracking-system/server/routes/pipelineRoutes.js
+++ job-tracking-system/server/routes/pipelineRoutes.js
@@ -1,42 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const pipelineController = require('../controllers/pipelineController');
 
 // @route   GET /api/pipelines
 // @desc    Get all pipelines
-// @access  Private
-router.get(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  pipelineController.getPipelines
-);
+router.get('/', pipelineController.getPipelines);
 
 // @route   POST /api/pipelines
 // @desc    Create a pipeline
-// @access  Private
-router.post(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  pipelineController.createPipeline
-);
+router.post('/', pipelineController.createPipeline);
 
 // @route   PUT /api/pipelines/:id
 // @desc    Update a pipeline
-// @access  Private
-router.put(
-  '/:id',
-  passport.authenticate('jwt', { session: false }),
-  pipelineController.updatePipeline
-);
+router.put('/:id', pipelineController.updatePipeline);
 
 // @route   DELETE /api/pipelines/:id
 // @desc    Delete a pipeline
-// @access  Private
-router.delete(
-  '/:id',
-  passport.authenticate('jwt', { session: false }),
-  pipelineController.deletePipeline
-);
+router.delete('/:id', pipelineController.deletePipeline);
 
 module.exports = router;
