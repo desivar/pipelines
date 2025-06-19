5615d13514c1353cc4b589daab2ee88bd80b9406
desivar <jillian_desire@yahoo.es>
2025-06-19 07:00:36 -0600

    /passport.js

---
+++ job-tracking-system/server/config/passport.js
@@ -0,0 +1,73 @@
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK_URL
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ githubId: profile.id });
+
+        if (!user) {
+          // Create new user
+          user = new User({
+            githubId: profile.id,
+            name: profile.displayName || profile.username,
+            email: profile.emails ? profile.emails[0].value : `${profile.username}@github.com`,
+            avatar: profile.photos ? profile.photos[0].value : '',
+            githubUsername: profile.username
+          });
+
+          await user.save();
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
+
+// JWT strategy
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+};
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      const user = await User.findById(jwt_payload.id);
+
+      if (user) {
+        return done(null, user);
+      }
+      return done(null, false);
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
