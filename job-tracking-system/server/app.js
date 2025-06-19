1f288b86d6f41c44c5400086305458716f0f779d
desivar <jillian_desire@yahoo.es>
2025-06-19 10:01:23 -0600

    up

--- job-tracking-system/server/app.js
+++ job-tracking-system/server/app.js
@@ -1,34 +1,14 @@
 const express = require('express');
-const cors = require('cors');
-const passport = require('passport');
-const connectDB = require('./config/db');
-const jobRoutes = require('./routes/jobRoutes');
-require('dotenv').config();
-require('./config/passport');
-
-const authRoutes = require('./routes/authRoutes');
-// ... other middleware ...
-app.use('/api/auth', authRoutes);
-
-// Connect to database
-connectDB();
-
-// Initialize app
 const app = express();
+const jobRoutes = require('./routes/jobRoutes');
 
 // Middleware
 app.use(express.json());
-app.use(cors());
-app.use(passport.initialize());
-app.use('/api/jobs', jobRoutes);
 
 // Routes
-app.use('/auth', require('./routes/authRoutes'));
-app.use('/api/pipelines', require('./routes/pipelineRoutes'));
-app.use('/api/jobs', require('./routes/jobRoutes'));
-app.use('/api/customers', require('./routes/customerRoutes'));
+app.use('/api/jobs', jobRoutes);
 
-// Error handling
+// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
