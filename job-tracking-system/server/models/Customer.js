c88bda8fa85a57f9e88dd4faae904ba560056845
desivar <jillian_desire@yahoo.es>
2025-06-19 06:54:10 -0600

    r/models/Customer.js

---
+++ job-tracking-system/server/models/Customer.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const CustomerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: String
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Customer', CustomerSchema);
