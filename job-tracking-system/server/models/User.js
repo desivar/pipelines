74dad3aa59352d710ddafd6d5426236ec3ce416e
desivar <jillian_desire@yahoo.es>
2025-06-19 09:22:29 -0600

    models/user simplifyed

--- job-tracking-system/server/models/User.js
+++ job-tracking-system/server/models/User.js
@@ -1,31 +1,10 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  githubId: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  avatar: {
-    type: String
-  },
-  githubUsername: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  name: String,
+  email: { type: String, unique: true },
+  githubId: String,
+  avatar: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
